2f724def1f162487d5e362c227ff0402e91f6b40
Angel Barrios <you@example.com>
2024-04-08 09:14:52 -0500

    Refactor Letter component in frontend/src/components/Letter.js

--- frontend/src/components/Letter.js
+++ frontend/src/components/Letter.js
@@ -4,33 +4,43 @@ export default function Letter({
   wordIndex,
   typeboxState
 }) {
-  const isCurrentLetter = (
-    wordIndex === typeboxState.wordIndex &&
-    letterIndex === typeboxState.letterIndex
-  );
-  const isPastLetter = (
-    wordIndex < typeboxState.wordIndex ||
-    (wordIndex === typeboxState.wordIndex && letterIndex < typeboxState.letterIndex)
-  );
-
-  const isIncorrect = typeboxState.incorrects.find(([incorrectWordIndex, incorrectLetterIndex]) => {
-    console.table({
-      incorrectWordIndex,
-      incorrectLetterIndex,
-      wordIndex,
-      letterIndex
-    });
+  const isCurrentLetter = () => {
     return (
-      wordIndex === incorrectWordIndex && letterIndex === incorrectLetterIndex
-    )
-  });
+      wordIndex === typeboxState.wordIndex &&
+      letterIndex === typeboxState.letterIndex
+    );
+  };
+
+  const isPastLetter = () => {
+    return (
+      wordIndex < typeboxState.wordIndex ||
+      (
+        wordIndex === typeboxState.wordIndex &&
+        letterIndex < typeboxState.letterIndex
+      )
+    );
+  };
+
+  const isIncorrect = () => {
+    return typeboxState.incorrects.find(([incorrectWordIndex, incorrectLetterIndex]) => {
+      console.table({
+        incorrectWordIndex,
+        incorrectLetterIndex,
+        wordIndex,
+        letterIndex
+      });
+      return (
+        wordIndex === incorrectWordIndex && letterIndex === incorrectLetterIndex
+      )
+    })
+  };
 
   let letterClassName = '';
-  if (isCurrentLetter) {
+  if (isCurrentLetter()) {
     letterClassName = 'active';
-  } else if (isPastLetter) {
+  } else if (isPastLetter()) {
     letterClassName = 'correct';
-  } else if (isIncorrect) {
+  } else if (isIncorrect()) {
     letterClassName = 'incorrect';
   }
 
